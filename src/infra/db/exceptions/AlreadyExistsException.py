66c083ad1476ee0ec806ac8814203d2150d86a74
Thrashattack <henrique202@gmail.com>
2024-06-15 04:41:54 +0200

    fix: change exception parent class

--- src/infra/db/exceptions/AlreadyExistsException.py
+++ src/infra/db/exceptions/AlreadyExistsException.py
@@ -1,4 +1,4 @@
-class AlreadyExistsException(Exception):
+class AlreadyExistsException(BaseException):
     def __init__(self, entity: str, id: str):
         super(AlreadyExistsException, self).__init__(
             f'Entity {entity} already exists with identifier {id}')
--- src/infra/db/exceptions/NotFoundException.py
+++ src/infra/db/exceptions/NotFoundException.py
@@ -1,4 +1,4 @@
-class NotFoundException(Exception):
+class NotFoundException(BaseException):
     def __init__(self, entity: str, id: str):
         super(NotFoundException, self).__init__(
             f'Not found entity {entity} with identifier {id}. Check Name and Password.')
